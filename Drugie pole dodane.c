98b3b99b5c7921b6653751a34529c17abac896bf
SL <sebix19971@gmail.com>
2021-03-30 14:36:00 +0200

    Dodanie funkcji i zabezpieczen

--- Drugie pole dodane.c
+++ Drugie pole dodane.c
@@ -1,63 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+
+float pole_prostokata();
+float pole_trojkata();
 
 int main()
 {
-
-
 for(;;)
 {
-int c=68 ;
-float a;
-float b;
-float wynik;
-float pods;
-float h;
-float wynik2;
 
-printf("wybierz cos\n");
+int c=68 ;
+
+printf("Wybierz bryle\n");
 printf("1.pole prostokata\n");
 printf("2.pole trojkata\n");
 printf("3. wyjscie\n");
-scanf("%d", &c);
+c =getch();
 switch (c)
 
 {
 
-case 1:
+case '1':
+pole_prostokata();
+for(int i= 0; i<=2;i++)
+    {
+        printf("\n");
+    };
+break;
 
+case '2':
+pole_trojkata();
+
+    printf("\n");
+
+break;
+
+case '3':
+exit(EXIT_SUCCESS);
+
+
+default:
+    printf("Wybierz cyfre od 1 do 3\n");
+    printf("Aby powrocic do menu wcisnij dowolny przycisk\n");
+    //exit(EXIT_SUCCESS);
+    getch();
+
+        printf("\n");
+
+break;
+}
+
+}
+    return 0;
+};
+
+float pole_prostokata()
+{
+    float a, b, wynik ;
     printf("wprowadz a\n");
-    scanf("%f", &a);
-    printf("wprowadz b\n");
-    scanf("%f", &b);
-    if ((a>0) && (b>0))
+    if(scanf("%f", &a) == 1)
     {
-    wynik = a*b;
-    printf("wynik=" "%f\n", wynik);
-    }
-else
-    printf("liczby ujemne!!!\n");
-    break;
-case 2:
+        printf("wprowadz b\n");
+        if(scanf("%f", &b) == 1)
+        {
 
-printf("Wprowadz podstawe\n");
-    scanf("%f", &pods);
-    printf("Wprowadz wysokosc\n");
-    scanf("%f", &h);
-    if ((pods>0)&&(h>0))
+
+            if ((a<=0) || (b<=0))
+            {
+            printf("a i b musza byc wieksze od 0\n");
+            exit (EXIT_FAILURE);
+
+            }
+
+            else
+
+
+            wynik = a*b;
+            printf("wynik= ""%.4f", wynik);
+            return wynik;
+        }
+        else
+        printf("wprowadz cyfry, a nie znaki\n");
+        exit (EXIT_FAILURE);
+
+    }
+    else
+
+   printf("wprowadz cyfry, a nie znaki\n");
+    exit (EXIT_FAILURE);
+
+
+};
+
+
+
+
+float pole_trojkata()
+{
+    float a, h, wynik;
+    printf("wprowadz a\n");
+    if(scanf("%f", &a) == 1)
     {
-        wynik2 = 0.5*pods*h;
-        printf("wynik=" "%f\n", wynik2);
+        printf("wprowadz b\n");
+        if(scanf("%f", &h) == 1)
+        {
+
+
+            if ((a<=0) || (h<=0))
+            {
+            printf("a i h musza byc wieksze od 0\n");
+            exit (EXIT_FAILURE);
+
+            }
+
+            else
+
+
+            wynik =0.5*a*h;
+            printf("wynik= ""%.4f", wynik);
+            return wynik;
+        }
+        else
+        printf("wprowadz cyfry, a nie znaki\n");
+        exit (EXIT_FAILURE);
+
     }
- else
-    printf("liczby ujemne!!!\n");
-    break;
-case 3:
-    return 0;
-}
+    else
 
- }
+    printf("wprowadz cyfry, a nie znaki\n");
+    exit (EXIT_FAILURE);
 
-    return 0;
 
-}
+};
+
